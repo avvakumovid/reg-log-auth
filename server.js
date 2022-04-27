ec7f3689360b72dacb4b55598768730b682a3398
Ivan Avvakumov <66466689+avvakumovid@users.noreply.github.com>
2022-04-27 11:26:51 +0300

    Refactor

--- server.js
+++ server.js
@@ -1,22 +1,37 @@
-import express from 'express'
-import cors from 'cors' 
-import 'dotenv/config'
-import mongoose from 'mongoose'
-import authRouter from './src/routes/authRouter.js'
+// import express from 'express'
+// import cors from 'cors' 
+// import 'dotenv/config'
+// import mongoose from 'mongoose'
+// import authRouter from './src/routes/authRouter.js'
+// const app = express();
+// app.use(cors());
+// app.use(express.json());
+// app.use('/auth', authRouter)
+// const PORT = process.env.PORT || 4000
+// const start = async () => {
+//     try {
+//         await mongoose.connect(process.env.CONNECTING_STRING)
+//         app.listen(PORT, () => console.log(`Server started on PORT ${PORT}...`))
+//     } catch (e) {
+//         console.log(e)
+//     }
+// }
+// start()
 
-const app = express();
-app.use(cors());
-app.use(express.json());
-app.use('/auth', authRouter)
+import {Router} from 'express';
+import AuthController from '../controller/authController.js'
+import {check} from 'express-validator';
+import authMiddleware from '../middleware/authMiddleware.js';
 
 
-const PORT = process.env.PORT || 4000
-const start = async () => {
-    try {
-        await mongoose.connect(process.env.CONNECTING_STRING)
-        app.listen(PORT, () => console.log(`Server started on PORT ${PORT}...`))
-    } catch (e) {
-        console.log(e)
-    }
-}
-start()
+const router = new Router()
+
+router.post('/registration', [
+    check('username', 'Имя пользлваьеля не может быть пустым').notEmpty(),
+    check('password', 'Пароль должен быть больше 4 символов').isLength({min: 4})
+], AuthController.registration)
+router.post('/login', AuthController.login)
+router.get('/auth', authMiddleware, AuthController.auth)
+
+
+export default router
--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -5,7 +5,6 @@ export default function (req, res, next) {
         next()
     }
     try {
-
         const token = req.headers.authorization.split(' ')[1]
         if (!token) {
             return res.status(403).json({message: 'Пользователь не авторизован'})
